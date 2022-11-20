9653c54fa513fad28ea34349b57e287d6f75ac57
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-11-20 21:22:16 +0800

    Add ui logic

--- app/src/main/java/com/example/basic_form_kt/MainActivity.kt
+++ app/src/main/java/com/example/basic_form_kt/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.basic_form_kt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        var messagePlainTxt = findViewById(R.id.messagePlainTxt) as EditText
+        var submitBtn = findViewById(R.id.submitBtn) as Button
+
+        submitBtn.setOnClickListener {
+            Toast.makeText(this, messagePlainTxt.getText().toString(), Toast.LENGTH_SHORT).show()
+            messagePlainTxt.setText("")
+        }
     }
 }
